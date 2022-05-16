e4b3ab3300b7a37d855fbd9872be87e33104a9d2
levigun <t.levina31@gmail.com>
2022-05-16 21:54:22 +1000

    edited navbar styling for log in

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -30,13 +30,13 @@ function Nav() {
             </Link>
           </li>
           <li className="nav-item mx-1">
-            <Link to="/orderHistory">
+            <Link to="/orderHistory" className="nav-link">
               Order History
             </Link>
           </li>
           <li className="nav-item mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
+            <a className="nav-link" href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
           </li>
